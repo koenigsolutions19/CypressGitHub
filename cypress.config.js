80c9a90fc76c264227c8295ef79f4c81f3d8c4be
satish <satishgang20@gmail.com>
2026-05-26 02:04:16 +0530

    fifth commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  pageLoadTimeout: 120000,
   allowCypressEnv: false,
 
   e2e: {
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
